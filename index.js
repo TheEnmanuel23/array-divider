1de3a095922849757a6a0cf07b8133f4e1c20e2d
Enmanuel Jarquin <enmisac@gmail.com>
2018-02-13 23:43:19 -0600

    Create a function that divide main array in chunks, and I added unit test

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const arrayChunks = (arrayMain=[], chunkLength=0) => {
+  if (!Array.isArray(arrayMain)) throw new TypeError('Expected array to split');
+  if (isNaN(chunkLength)) throw new TypeError('A number was expected as a second parameter');
+
+  let chunkAmount = Math.ceil(arrayMain.length/chunkLength);
+
+  let start = 0, newArr = [];
+
+  for (let i = 0; i < chunkAmount; i++) {
+      let arrToWork = arrayMain.slice(0);
+      newArr.push(arrToWork.splice(start, chunkLength));
+      start += chunkLength;
+  }
+
+  return newArr;
+}
+
+module.exports = arrayChunks;
---
+++ test/index.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const arrayChunks = require('../');
+const mocha = require('mocha');
+const expect = require('chai').expect;
+
+describe('array chunks module', () => {
+  describe('validate return types', () => {
+    it('should export a function', () => {
+        expect(arrayChunks).to.be.a('function');
+    });
+
+    it('return new array', () => {
+        expect(arrayChunks()).to.be.a('array');
+    });
+
+    it('with the empty array as first param', () => {
+        expect(arrayChunks([])).to.be.a('array');
+    });
+
+    it('with the two parameters', () => {
+        expect(arrayChunks([], 2)).to.be.a('array');
+    });
+  });
+
+  describe('validate params types', () => {
+    it('should return error, first parameter should to be a array', () => {
+      expect(() => arrayChunks(12)).to.throw(TypeError);
+    });
+
+    it('should return error, second parameter should to be a number', () => {
+        expect(() => arrayChunks(NaN)).to.throw(TypeError);
+    });
+  });
+
+  describe('split array', () => {
+      it('split simple array', () => {
+          let arrayBefore = [1,2,3,4,5,6];
+          let arrayAfter = [[1,2], [3,4], [5,6]];
+          expect(arrayChunks(arrayBefore, 2)).to.eql(arrayAfter);
+      });
+
+      it('last chunk with shorter length', () => {
+        let arrayBefore = [1,2,3,4,5,6,7];
+        let arrayAfter = [[1,2], [3,4], [5,6], [7]];
+        expect(arrayChunks(arrayBefore, 2)).to.eql(arrayAfter);        
+      });
+
+      
+  });
+});
